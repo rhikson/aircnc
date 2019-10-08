8f5e0cf941402d6319d070adddf5c59201038528
Rhikson Medeiros <rhiksonmedeiros@gmail.com>
2019-10-07 23:52:24 -0300

    Add socket.io and user capture started.

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,13 +3,37 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
+
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect('mongodb+srv://oministack09:oministack09@cluster0-7kyw0.mongodb.net/semana09?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    // console.log('Usuário conectado', socket.id);
+    const { user_id } = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.io;
+});
+
+//Adiciona uma funcionalidade em todas as rotas
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers; 
+    
+    return next();
+});
+
 //GET, POST, PUT, DELETE
 
 // req.query = Acessar query params (para filtros)
@@ -20,4 +44,4 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
-app.listen(3333);
+server.listen(3333);
